ac8f2ae4f9814036501a8412aaab555b03490c44
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-01-01 16:08:03 +0300

    Static files added and edited

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const express = require('express')
+const express = require('express');
+const path=require('path')
+const app = express();
 
-const app = express()
+const ejs = require('ejs')
 
-const port = 3001
+app.set("view engine", "ejs")
+app.use(express.static('public'));
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
+const port = 3001;
+
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
 app.get('/', (req, res) => {
-    res.send(blog)
-})
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
